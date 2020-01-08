018fce6a99e3f602f002c445a61f4d9f70bf7bb8
Jacob Beneski <jacobbeneski@gmail.com>
2020-01-07 20:25:17 -0600

    Add a decent about paragraph.

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,9 +1,11 @@
 <template>
     <b-container class="main-content">
         <b-img src="../assets/picture.jpg" rounded="circle" alt="Picture of Jacob Beneski" left></b-img>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent augue magna, bibendum a nisl in, tincidunt rhoncus tortor. Integer varius elit et enim mollis, eu luctus eros tempor. Ut et orci finibus, laoreet nunc nec, lobortis est. Proin mi lacus, tristique at ultrices a, convallis non mi. Vivamus ac nisl ante. Aenean at imperdiet elit. Nunc malesuada velit erat, ultrices egestas lectus iaculis eu. Sed sed fermentum felis. Curabitur eget dignissim libero, feugiat sollicitudin purus. Nam tempor varius leo, id tincidunt velit porta vitae.
+        <p>Some of my earliest memories involve taking things apart to see how they work. I have always loved diving into the inner workings of anything I can get my hands on. When I was old enough to have my own computer, this desire to learn grew exponentially and I became interested in programming shortly after.</p>
 
-        Quisque volutpat neque erat, vel consectetur ipsum egestas id. Aliquam ullamcorper tellus non nisi efficitur sollicitudin. Mauris condimentum nunc in pellentesque vehicula. Phasellus ut orci suscipit, dapibus neque vel, pretium nisi. Donec auctor purus ac massa congue, eu feugiat nulla cursus. Maecenas dui tortor, luctus quis feugiat eget, vulputate vitae leo. In cursus at dolor id faucibus. Nam accumsan rhoncus sem rutrum mattis. Ut id pulvinar urna, ultricies laoreet mauris. Nam aliquet ipsum at urna laoreet rhoncus. Cras eleifend iaculis nulla venenatis lacinia.
+        <p>My formal software education started in my Junior year of high school, and I completed my Computer Science degree from St. Edwards in December of 2019. To satisfy my addiction to leaning, I continued to explore new concepts, tools and programming languages alongside my classwork.</p>
+
+        <p>TL;DR: I'm a hard worker, and I enjoy to learn, but I also like to have fun and relax! Let's grab a beer and talk!</p>
     </b-container>
 </template>
 
